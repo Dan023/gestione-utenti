e695c07ad4e072bac531d6a8f54debdf72ac5bac
Daniele <danielecaterina2002@gmail.com>
2023-03-06 17:18:12 +0100

    cors

--- src/main/java/it/infobasic/gestioneutenti/controllers/UtenteController.java
+++ src/main/java/it/infobasic/gestioneutenti/controllers/UtenteController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import it.infobasic.gestioneutenti.models.Utente;
 import it.infobasic.gestioneutenti.services.UtenteService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 @RequestMapping("/api")
 public class UtenteController {
